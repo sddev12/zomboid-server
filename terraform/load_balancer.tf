c0bdbe175294a964d46de3d3daf6c127ca8bf87c
sddev12 <sd.dev@outlook.com>
2024-03-31 17:15:58 +0100

    set up load balancer

---
+++ terraform/load_balancer.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "nlb" {
+  name               = "zomboid-server-nlb"
+  load_balancer_type = "network"
+
+  subnet_mapping {
+    subnet_id     = aws_subnet.public.id
+    allocation_id = aws_eip.eip.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "port_16261" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "16261"
+  protocol          = "UDP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.zomboid_server_16261.arn
+  }
+}
+
+resource "aws_lb_listener" "port_16262" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "16262"
+  protocol          = "UDP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.zomboid_server_16262.arn
+  }
+}
+
+resource "aws_lb_target_group" "zomboid_server_16261" {
+  name     = "zomboid-server-lb-tg-16261"
+  port     = 16261
+  protocol = "UDP"
+  vpc_id   = aws_vpc.zomboid_vpc.id
+}
+
+resource "aws_lb_target_group" "zomboid_server_16262" {
+  name     = "zomboid-server-lb-tg-16262"
+  port     = 16262
+  protocol = "UDP"
+  vpc_id   = aws_vpc.zomboid_vpc.id
+}
